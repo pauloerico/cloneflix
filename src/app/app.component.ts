44c12cd09648aa91dd9380cfc3e2536b03b5d6a4
Paulo <deoliveira.pe@gmail.com>
2023-05-11 02:19:02 -0300

    feat: cloneflix app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,44 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  menuOpened: boolean = false;
   
+  listTitles = [{
+    trailer: 'assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
+    cover: 'assets/images/guardios-da-galaxia.jpg',
+    name: 'Guardiões da Galaxia',
+    relevance: 98,
+    age: 0,
+    parts: 2,
+    categories: ['Épico', 'Fantasia', 'Viagem no espaço']
+  }, {
+    trailer: 'assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
+    cover: 'assets/images/guardios-da-galaxia.jpg',
+    name: 'Guardiões da Galaxia Vol. 2',
+    relevance: 80,
+    age: 12,
+    parts: 2,
+    categories: ['Épico', 'Fantasia', 'Viagem no espaço']
+  }, {
+    trailer: 'assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
+    cover: 'assets/images/guardios-da-galaxia.jpg',
+    name: 'Guardiões da Galaxia Vol. 3',
+    relevance: 55,
+    age: 18,
+    parts: 2,
+    categories: ['Épico', 'Fantasia', 'Viagem no espaço']
+  }];
+  
+  setMenuState(state: boolean) {
+    this.menuOpened = state;
+  }
+
+  closeMenu(){
+    this.menuOpened = false;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
+import { FullBannerComponent } from './shared/full-banner/full-banner.component';
+import { ListTitlesComponent } from './shared/list-titles/list-titles.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { TitleComponent } from './shared/title/title.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FullBannerComponent,
+    ListTitlesComponent,
+    FooterComponent,
+    TitleComponent
   ],
   imports: [
     BrowserModule
